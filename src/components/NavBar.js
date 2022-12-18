dec71d5c71ee29bc336c865f9572548931950009
notandy82 <notandystanek@gmail.com>
2022-12-18 21:57:11 +0000

    Style navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import { Navbar, Container, Nav } from "react-bootstrap"
+import styles from "../styles/NavBar.module.css"
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar className={styles.NavBar} expand="lg" fixed="top">
       <Container>
-        <Navbar.Brand href="#home">The Hearth of the Dungeon</Navbar.Brand>
+        <Navbar.Brand className={styles.Title}>The Hearth of the Dungeon</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>      
+          <Nav className="ml-auto text-left">
+            <Nav.Link>
+              <i className="fas fa-house-user"></i>Home
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-user-plus"></i>Sign up
+            </Nav.Link>
           </Nav>    
         </Navbar.Collapse>
       </Container>
