8f46fc22c2c8b5fdef311373244c28c7c91b2e12
notandy82 <notandystanek@gmail.com>
2022-12-18 17:18:45 +0000

    Add navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Navbar, Container, Nav } from "react-bootstrap"
+
+const NavBar = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">The Hearth of the Dungeon</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>      
+          </Nav>    
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default NavBar
