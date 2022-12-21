b81608d65379580ead4354256d8dbe1b7cba1a95
notandy82 <notandystanek@gmail.com>
2022-12-21 10:38:58 +0000

    Rename contexts folder, begin building profile page

---
+++ src/pages/profiles/Profile.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from '../../styles/Profile.module.css';
+import btnStyles from '../../styles/Button.module.css';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+
+const Profile = (props) => {
+  const { profile, mobile, imageSize=55 } = props;
+  const { id, following_id, image, owner } = profile;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+  return (
+    <div className={`my-3 d-flex align-items-center ${mobile && "flex-Column"}`}>
+      <div>
+        <Link className="align-self-center" to={`/profiles/${id}`}>
+            Avatar
+        </Link>
+      </div>
+      <div className={``}></div>
+    </div>
+  )
+}
+
+export default Profile
