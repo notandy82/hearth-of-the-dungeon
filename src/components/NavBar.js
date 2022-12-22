60e244cba9682849b0a69e88f0b4bf6f71723f17
notandy82 <notandystanek@gmail.com>
2022-12-22 11:42:43 +0000

    Add toggling to navbar for mobile devices

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,11 +9,14 @@ import {
 import axios from "axios";
 import { removeTokenTimestamp } from "../utils/utils";
 import Avatar from "./Avatar";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const {expanded, setExpanded, ref} = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -74,7 +77,12 @@ const NavBar = () => {
     </>
   );
   return (
-    <Navbar className={styles.NavBar} expand="lg" fixed="top">
+    <Navbar
+      expanded={expanded}
+      className={styles.NavBar}
+      expand="lg"
+      fixed="top"
+    >
       <Container>
         <NavLink to="/">
           <Navbar.Brand className={styles.Title}>
@@ -82,7 +90,11 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPartyIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
