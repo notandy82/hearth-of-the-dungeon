4f862bb13b8366d9d97b10c33c641cf17ea95b0d
notandy82 <notandystanek@gmail.com>
2022-12-22 22:29:34 +0000

    Create party page to display parties

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PartyCreateForm from "./pages/parties/PartyCreateForm";
+import PartyPage from "./pages/parties/PartyPage";
 
 
 
@@ -20,6 +21,7 @@ function App() {
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/parties/create" render={() => <PartyCreateForm />} />
+          <Route exact path="/parties/:id" render={() => <PartyPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
--- src/pages/parties/PartyPage.js
+++ src/pages/parties/PartyPage.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function PostPage() {
-  // Add your logic here
+  const { id } = useParams();
+  const [party, setParty] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{data: post}] = await Promise.all([
+          axiosReq.get(`/parties/${id}`),
+        //   axiosReq.get(`/posts/?party=${id}`),
+        //   axiosReq.get(`/comments/?post=${id}`),
+        ])
+        setParty({results: [party]})
+        console.log(party)
+      } catch(err){
+        console.log(err)
+      }
+    }
+
+    handleMount()
+  }, [id]);
 
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular parties for mobile</p>
-        <p>Post component</p>
+        <p>Party component</p>
         <Container className={appStyles.Content}>
           Comments
         </Container>
