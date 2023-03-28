a23e6957a821f30f7628c9cd041bae1c6b6ba861
notandy82 <notandystanek@gmail.com>
2023-03-28 18:05:22 +0000

    Add post data to post page

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Avatar from '../../components/Avatar';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import styles from '../../styles/Post.module.css';
 
@@ -26,7 +28,20 @@ const Post = (props) => {
   return (
     <Card className={styles.Post}>
       <Card.Body>
-        
+        <Avatar src={profile_image} height={55} />
+        {owner}
+        <div className="d-flex align-items-center">
+          <span>{updated_at}</span>
+        </div>
+      </Card.Body>
+      <Card.Body>
+        <Link to={`/posts/${id}`}>
+          <Card.Img src={image} alt={title} />
+        </Link>
+      </Card.Body>
+      <Card.Body>
+        {title && <Card.Title className="text-center">{title}</Card.Title>}
+        {content && <Card.Text>{content}</Card.Text>}
       </Card.Body>
     </Card>
   )
