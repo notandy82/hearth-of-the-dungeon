1bc4ed8fc2a5a8c0486aea7cd61999de01f6cffe
notandy82 <notandystanek@gmail.com>
2023-03-29 14:02:20 +0000

    Connect post container to party page

--- src/pages/posts/PostContainer.js
+++ src/pages/posts/PostContainer.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import Post from './Post'
+import React from 'react';
+import { Col, Row } from 'react-bootstrap'
+import Post from './Post';
 
 const PostContainer = (props) => {
   return (
-    <div>
-      <Post />
-    </div>
+    <Row className="h100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>List of posts here</p>
+      </Col>
+    </Row>
   )
 }
 
-export default PostContainer
+export default PostContainer;
